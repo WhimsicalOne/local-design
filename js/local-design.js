8d84f57bac96cf5d0d8f4f55e88cf45f16a2ce50
WhimsicalOne <42167184+WhimsicalOne@users.noreply.github.com>
2018-08-12 09:06:05 +0200

    Add files via upload

---
+++ js/local-design.js
@@ -0,0 +1,9 @@
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+  $(document).ready(function(){               
+  $('.materialboxed').materialbox();
+});
+$(document).ready(function(){
+  $('.materialboxed').materialbox();
+});
